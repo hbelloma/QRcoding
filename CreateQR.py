71599056c8bb3c3dc347e8116b44798c379de1a0
Hector Bello Martinez <62455718+hbelloma@users.noreply.github.com>
2020-04-16 13:55:48 -0500

    Update CreateQR.py

--- CreateQR.py
+++ CreateQR.py
@@ -1,8 +1,20 @@
 #! /usr/bin/python3
 # to do first install: pip install qrcode[pil]
-
 import qrcode
-img = qrcode.make(input('What should the QR code say? '))
+
+qr = qrcode.QRCode(
+	version=1,
+	box_size=15,
+	border=5
+)
+data = 'your text here'
+qr.add_data(data)
+qr.make(fit=True)
+img = qr.make_image(fill='black', back_color='white')
+
+#you can Skip all behind with the following line
+#img = qrcode.make(input('What should the QR code say? '))
 img.save('saved.png')
 
 img.show('saved.png') #You can remove this line if you do not need to see a preview after QR generation
+
